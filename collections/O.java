57fd9a58d125435b9326f530d89c1ffd23e59d09
Dinesh S <111220091+dineshs-27@users.noreply.github.com>
2022-10-27 22:38:55 +0530

    Add files via upload

---
+++ collections/O.java
@@ -0,0 +1,33 @@
+package LittleAlchemyCombinations.collections;
+
+import LittleAlchemyCombinations.Abstract;
+
+import LittleAlchemyCombinations.Combination;
+
+import java.util.Scanner;
+
+public class O extends Abstract{
+    @Override
+    public void alphabet() {
+        System.out.println("Lake\nLamp\nLand\nLaptop\nLasso\nLava\nLava Lamp\nLawn\nLawn Mower\nLeaf\nLeather\nLegend\nLens\nLetter\nLibrarian\nLibrary\nLife\nLight\nLight Bulb\nLight Sword\nLighthouse\nLightning\nLion\nLiquid\nLittle Alchemy\nLivestock\nLizard\nLog Cabin\nLove\nLumberjack");
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the word: ");
+        String o = input.nextLine();
+        switch (o) {
+            case "oasis" -> Combination.oasis();
+            case "obsidian" -> Combination.obsidian();
+            case "ocean" -> Combination.ocean();
+            case "oil" -> Combination.oil();
+            case "omelette" -> Combination.omelette();
+            case "optical fiber" -> Combination.opticalfibre();
+            case "orchard" -> Combination.orchard();
+            case "ore" -> Combination.ore();
+            case "organic matter" -> Combination.organicmatter();
+            case "origami" -> Combination.origami();
+            case "ostrich" -> Combination.ostrich();
+            case "owl" -> Combination.owl();
+            case "oxygen" -> Combination.oxygen();
+            case "ozone" -> Combination.ozone();
+        }
+    }
+}
