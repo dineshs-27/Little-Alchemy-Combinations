b5b441a9276df5132ad1b88ddf7767e0207fa364
Dinesh S <111220091+dineshs-27@users.noreply.github.com>
2022-10-27 22:37:49 +0530

    Add files via upload

---
+++ collections/K.java
@@ -0,0 +1,24 @@
+package LittleAlchemyCombinations.collections;
+
+import LittleAlchemyCombinations.Abstract;
+
+import LittleAlchemyCombinations.Combination;
+
+import java.util.Scanner;
+
+public class K extends Abstract{
+    @Override
+    public void alphabet() {
+        System.out.println("Kaiju\nKatana\nKite\nKnife\nKnight");
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the word: ");
+        String k = input.nextLine();
+        switch (k) {
+            case "kaiju" -> Combination.kaiju();
+            case "katana" -> Combination.katana();
+            case "kite" -> Combination.kite();
+            case "knife" -> Combination.knife();
+            case "knight" -> Combination.knight();
+        }
+    }
+}
