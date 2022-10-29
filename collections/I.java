530eff64057558e8498ed72397c75f5ce2945aa8
Dinesh S <111220091+dineshs-27@users.noreply.github.com>
2022-10-29 11:56:03 +0530

    Update I.java

--- collections/I.java
+++ collections/I.java
@@ -9,17 +9,17 @@ import java.util.Scanner;
 public class I extends Abstract {
     @Override
     public void alphabet() {
-        System.out.println("Ice\nIce Cream\nIce Cream Truck\nIce Sculpture\nIceberg\nIced Tea\nIdea\nIgloo\nInternet\nIsland\nIvy");
+        System.out.println("ice\nicecream\nicecreamtruck\nicesculpture\niceberg\nicedtea\nidea\nigloo\ninternet\nisland\nivy");
         Scanner input = new Scanner(System.in);
         System.out.println("Enter the word: ");
         String i = input.nextLine();
         switch (i){
             case "ice" -> Combination.ice();
-            case "ice cream" -> Combination.icecream();
-            case "ice cream truck" -> Combination.icecreamtruck();
-            case "ice sculpture" -> Combination.icesculpture();
+            case "icecream" -> Combination.icecream();
+            case "icecreamtruck" -> Combination.icecreamtruck();
+            case "icesculpture" -> Combination.icesculpture();
             case "iceberg" -> Combination.iceberg();
-            case "iced tea" -> Combination.icedtea();
+            case "icedtea" -> Combination.icedtea();
             case "idea" -> Combination.idea();
             case "igloo" -> Combination.igloo();
             case "internet" -> Combination.internet();
